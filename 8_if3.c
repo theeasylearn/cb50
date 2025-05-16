809c3903f3e8f8c053c73204ce5d9b35677f7a02
theeasylearn <theeasylearn@gmail.com>
2025-05-16 09:16:15 +0530

    if decision making statement

---
+++ 8_if3.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+void main()
+{
+
+    int first_number,second_number;
+
+    printf("enter first number :");
+    scanf("%d",&first_number);
+    printf("enter second number :");
+    scanf("%d",&second_number);
+
+    if(first_number == 1)
+    {
+        printf("one");
+    }
+    if (first_number == 2)
+    {
+        printf("two");
+    }
+    if(first_number == 3)
+    {
+        printf("three");
+    }
+    if(first_number == 4)
+    {
+        printf("four");
+    }
+     if(first_number == 5)
+    {
+        printf("five");
+    }
+     if(first_number == 6)
+    {
+        printf("six");
+    }
+     if(first_number == 7)
+    {
+        printf("seven");
+    }
+     if(first_number == 8)
+    {
+        printf("eight");
+    }
+     if(first_number == 9)
+    {
+        printf("nine");
+    }
+     if(first_number == 0)
+    {
+        printf("zero");
+    }
+    else
+    {
+        printf("invalide number");
+    }
+
+    if(second_number == 1)
+    {
+        printf("one");
+    }
+    if (second_number == 2)
+    {
+        printf("two");
+    }
+    if(second_number == 3)
+    {
+        printf("three");
+    }
+    if(second_number == 4)
+    {
+        printf("four");
+    }
+     if(second_number == 5)
+    {
+        printf("five");
+    }
+     if(second_number == 6)
+    {
+        printf("six");
+    }
+     if(second_number == 7)
+    {
+        printf("seven");
+    }
+     if(second_number == 8)
+    {
+        printf("eight");
+    }
+     if(second_number == 9)
+    {
+        printf("nine");
+    }
+     if(second_number == 0)
+    {
+        printf("zero");
+    }
+      else
+    {
+        printf("invalide number");
+    }
+
+}
---
+++ 9_if4.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+void main ()
+{
+    int month_number;
+
+    printf("enter month_number");
+    scanf("%d",&month_number);
+
+    if(month_number == 1)
+    {
+        printf("jan");
+    }
+    if(month_number == 2)
+    {
+        printf("feb");
+    }
+    if(month_number == 3)
+    {
+        printf("march");
+    }
+     if(month_number == 4)
+    {
+        printf("april");
+    }
+     if(month_number == 5)
+    {
+        printf("may");
+    }
+     if(month_number == 6)
+    {
+        printf("june");
+    }
+     if(month_number == 7)
+    {
+        printf("july");
+    }
+     if(month_number == 8)
+    {
+        printf("aug");
+    }
+     if(month_number == 9)
+    {
+        printf("sep");
+    }
+     if(month_number == 10)
+    {
+        printf("oct");
+    }
+     if(month_number == 11)
+    {
+        printf("nov");
+    }
+     if(month_number == 12)
+    {
+        printf("dec");
+    }
+   
+    
+
+
+}
