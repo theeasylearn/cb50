156dc9b4e3974625b205ed69a12b748b1ac5dcf6
theeasylearn <theeasylearn@gmail.com>
2025-06-02 20:32:40 +0530

    test

---
+++ 32_count_odd_even.c
@@ -0,0 +1,23 @@
+/*
+    count no of odd and even value into integer array
+    steps
+    1) create & initialize array as numbers
+    2) create variable odd, even and store 0 into it
+    3) check value at 1st position is odd or even
+       if(number%2==0)
+        even = even  + 1
+       else
+        odd = odd + 1
+    4) check value at 2nd position is odd or even
+       if(number%2==0)
+        even = even  + 1
+       else
+        odd = odd + 1
+    5) check value at 3rd position is odd or even
+       if(number%2==0)
+        even = even  + 1
+       else
+        odd = odd + 1
+
+    write code as per steps
+*/
---
+++ 33_min_max.c
@@ -0,0 +1,17 @@
+/*
+    findout minimum value from  float array 
+    steps
+    1) create & initialize float array as numbers
+    2) create variable min 
+    3) store 1st value of array into min variable (assume 1st value is minimum value)
+    4) compare 2nd value of array with min variable if min variable has larger value then 2nd value then store 2nd value into min variable 
+       if(min>number[1])
+            min = number[1]
+    5)  if(min>number[2])
+            min = number[1]       
+    6)  if(min>number[3)
+            min = number[1]   
+    7)  if(min>number[4)
+            min = number[1]         
+    write code as per steps
+*/
